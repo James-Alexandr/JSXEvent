71795fe21efdc280877e650825b9ffd01ee0a860
James Alexander <60295314+James-Alexandr@users.noreply.github.com>
2021-06-21 11:09:54 -0500

    OnMouseEvents and state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [isMouseOver, setColor] = useState(false);
+
+  function handleClick() {
+    console.log("Clicked");
+  }
+
+  function onMouseOver() {
+    setColor(true);
+  }
+
+  function onMouseOut() {
+    setColor(false);
+  }
+
   return (
     <div className="container">
       <h1>Hello</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={onMouseOver}
+        onMouseOut={onMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
